0e65fc9bc0d0a50c94bdb07605bd41ec828a41a1
alexanderhmw <alexanderhmw@gmail.com>
2014-10-22 23:18:52 +0900

    Linux Install Doxygen

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get -y install qt5-default libboost-all-dev;
+sudo apt-get -y install qt5-default libboost-all-dev doxygen;
 mkdir -p GCC/Tools;cd GCC/Tools;
 qmake -makefile ../../Src/Tools/Tools.pro -r "CONFIG+=release";
 make;
@@ -12,3 +12,8 @@ make -f Makefile.release install;
 qmake ../../Src/Kernel/Kernel.pro -o Makefile.debug -r "CONFIG+=debug";
 make -f Makefile.debug;
 make -f Makefile.debug install;
+cd ..;cd ..;cd Doc/Doxygen;
+doxygen RobotSDK;
+cd ../html;
+mkdir -p $HOME/SDK/RobotSDK/Doc;
+cp * $HOME/SDK/RobotSDK/Doc;
